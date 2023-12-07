5aa48c707248eb0c14c84363d744b1137b2083b9
juanvidev <juanvidev1@gmail.com>
2023-12-07 12:46:29 -0500

    style: change size of close icon

--- src/components/DeleteIcon.js
+++ src/components/DeleteIcon.js
@@ -2,9 +2,13 @@ import React from "react";
 import { IoClose } from "react-icons/io5";
 
 const DeleteIcon = (props) => {
+    const size = {
+        width: "100%",
+        height: "100%"
+    }
     return (
     <span className="icon icon-delete" onClick={props.onDelete}>
-      <IoClose />
+      <IoClose style={size} />
     </span>
   );
 
